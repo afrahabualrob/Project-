831f8cfe6e952ff5f0b88c2c67962a7016d0c945
iamhaitham <iamhaitham2011@gmail.com>
2022-12-03 13:49:40 +0200

    Update styling for UploadSection

--- src/component/UploadSection/UploadSection.jsx
+++ src/component/UploadSection/UploadSection.jsx
@@ -2,20 +2,14 @@ import styles from "./UploadSection.module.css";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
 const UploadSection = () => {
-  const { customFileUpload, filePreview, wrapper } = styles;
+  const { customFileUpload, wrapper } = styles;
 
   return (
-    <div style={styles}>
-      <div className={wrapper}>
-        <label className={customFileUpload}>
-          <input type="file" multiple />
-          <CloudUploadIcon /> Attach
-        </label>
-
-        {/* {this.state.files.map(x => 
-           <div className={ filePreview } onClick={removeFile(e, x)}>{x.name}</div>
-      )} */}
-      </div>
+    <div className={wrapper}>
+      <label className={customFileUpload}>
+        <input type="file" multiple />
+        <CloudUploadIcon /> Attach
+      </label>
     </div>
   );
 };
